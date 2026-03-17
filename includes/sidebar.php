50ea2794bf1c683c738b401def9ba30e81d22f95
MyXuyenVo31 <myxuyenvo31@gmail.com>
2026-03-17 09:56:55 +0700

    remove old admin page

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,189 +1,102 @@
-<!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-  <!-- Sidebar - Brand -->
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
     <div class="sidebar-brand-icon rotate-n-15">
       <i class="fas fa-laugh-wink"></i>
     </div>
-    <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+    <div class="sidebar-brand-text mx-3">ADMIN PANEL</div>
   </a>
-
-  <!-- Divider -->
   <hr class="sidebar-divider my-0" />
-
-  <!-- Nav Item - Dashboard -->
   <li class="nav-item active">
-    <a class="nav-link" href="index.html">
+    <a class="nav-link" href="index.php">
       <i class="fas fa-fw fa-tachometer-alt"></i>
-      <span>Dashboard</span></a>
+      <span>Dashboard</span>
+    </a>
   </li>
-
-  <!-- Divider -->
   <hr class="sidebar-divider" />
-
-  <!-- Heading -->
-  <div class="sidebar-heading">Chức năng chính:</div>
+  <div class="sidebar-heading">Quản lý Sản phẩm</div>
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
-      aria-controls="collapseOne">
-      <i class="fas fa-calendar-day"></i>
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBrand" aria-expanded="true"
+      aria-controls="collapseBrand">
+      <i class="fas fa-tags"></i>
       <span>Thương hiệu - Brand</span>
     </a>
-    <div id="collapseOne" class="collapse" aria-labelledby="collapseOne" data-parent="#accordionSidebar">
+    <div id="collapseBrand" class="collapse" aria-labelledby="headingBrand" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Các chức năng:</h6>
-        <a class="collapse-item" href="./listbrands.php">Liệt kê</a>
+        <a class="collapse-item" href="./listbrands.php">Danh sách thương hiệu</a>
         <a class="collapse-item" href="./themthuonghieu.php">Thêm mới</a>
       </div>
     </div>
   </li>
-  <!-- Nav Item - Pages Collapse Menu -->
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-      aria-controls="collapseTwo">
-      <i class="fas fa-calendar-day"></i>
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCat" aria-expanded="true"
+      aria-controls="collapseCat">
+      <i class="fas fa-list"></i>
       <span>Danh mục sản phẩm</span>
     </a>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div id="collapseCat" class="collapse" aria-labelledby="headingCat" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Các chức năng:</h6>
-        <a class="collapse-item" href="./listcats.php">Liệt kê</a>
+        <a class="collapse-item" href="./listcats.php">Danh sách danh mục</a>
         <a class="collapse-item" href="./themdanhmuc.php">Thêm mới</a>
       </div>
     </div>
   </li>
-
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true"
-      aria-controls="collapseTwo">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseProd" aria-expanded="true"
+      aria-controls="collapseProd">
       <i class="fab fa-product-hunt"></i>
       <span>Sản phẩm</span>
     </a>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div id="collapseProd" class="collapse" aria-labelledby="headingProd" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Các chức năng:</h6>
-        <a class="collapse-item" href="./listsanpham.php">Liệt kê</a>
+        <a class="collapse-item" href="./listsanpham.php">Danh sách sản phẩm</a>
         <a class="collapse-item" href="./themsanpham.php">Thêm mới</a>
       </div>
     </div>
   </li>
-
+  <hr class="sidebar-divider" />
+  <div class="sidebar-heading">Quản lý Bài viết</div>
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseDMTT" aria-expanded="true"
-      aria-controls="collapseTwo">
-      <i class="fas fa-calendar-day"></i>
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseNewsCat" aria-expanded="true"
+      aria-controls="collapseNewsCat">
+      <i class="fas fa-folder-open"></i>
       <span>Danh mục tin tức</span>
     </a>
-    <div id="collapseDMTT" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div id="collapseNewsCat" class="collapse" aria-labelledby="headingNewsCat" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Các chức năng:</h6>
-        <a class="collapse-item" href="./listnewscats.php">Liệt kê</a>
+        <a class="collapse-item" href="./listnewscats.php">Danh sách danh mục</a>
         <a class="collapse-item" href="./themdanhmuctintuc.php">Thêm mới</a>
       </div>
     </div>
   </li>
-
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTT" aria-expanded="true"
-      aria-controls="collapseTwo">
-      <i class="fab fa-product-hunt"></i>
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseNews" aria-expanded="true"
+      aria-controls="collapseNews">
+      <i class="fas fa-newspaper"></i>
       <span>Tin tức</span>
     </a>
-    <div id="collapseTT" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div id="collapseNews" class="collapse" aria-labelledby="headingNews" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Các chức năng:</h6>
-        <a class="collapse-item" href="./listnews.php">Liệt kê</a>
+        <a class="collapse-item" href="./listnews.php">Danh sách bài viết</a>
         <a class="collapse-item" href="./themtintuc.php">Thêm mới</a>
       </div>
     </div>
   </li>
+  <hr class="sidebar-divider" />
+  <div class="sidebar-heading">Quản lý Bán hàng</div>
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true"
-      aria-controls="collapseTwo">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOrder" aria-expanded="true"
+      aria-controls="collapseOrder">
       <i class="fas fa-wallet"></i>
       <span>Đơn hàng</span>
     </a>
-    <div id="collapseFour" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div id="collapseOrder" class="collapse" aria-labelledby="headingOrder" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Các chức năng:</h6>
-        <a class="collapse-item" href="./listorders.php">Liệt kê</a>
-        <a class="collapse-item" href="cards.html">Thêm mới</a>
+        <a class="collapse-item" href="./listorders.php">Danh sách đơn hàng</a>
       </div>
     </div>
   </li>
-
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true"
-      aria-controls="collapseTwo">
-      <i class="fas fa-users"></i>
-      <span>Người dùng</span>
-    </a>
-    <div id="collapseFive" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Các chức năng:</h6>
-        <a class="collapse-item" href="buttons.html">Liệt kê</a>
-        <a class="collapse-item" href="cards.html">Thêm mới</a>
-      </div>
-    </div>
-  </li>
-
-  <!-- Divider -->
-  <hr class="sidebar-divider" />
-
-  <!-- Heading -->
-  <div class="sidebar-heading">Addons</div>
-
-  <!-- Nav Item - Pages Collapse Menu -->
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-      aria-controls="collapsePages">
-      <i class="fas fa-fw fa-folder"></i>
-      <span>Pages</span>
-    </a>
-    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Login Screens:</h6>
-        <a class="collapse-item" href="login.html">Login</a>
-        <a class="collapse-item" href="register.html">Register</a>
-        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-        <div class="collapse-divider"></div>
-        <h6 class="collapse-header">Other Pages:</h6>
-        <a class="collapse-item" href="404.html">404 Page</a>
-        <a class="collapse-item" href="blank.html">Blank Page</a>
-      </div>
-    </div>
-  </li>
-
-  <!-- Nav Item - Charts -->
-  <li class="nav-item">
-    <a class="nav-link" href="charts.html">
-      <i class="fas fa-fw fa-chart-area"></i>
-      <span>Charts</span></a>
-  </li>
-
-  <!-- Nav Item - Tables -->
-  <li class="nav-item">
-    <a class="nav-link" href="tables.html">
-      <i class="fas fa-fw fa-table"></i>
-      <span>Tables</span></a>
-  </li>
-
-  <!-- Divider -->
   <hr class="sidebar-divider d-none d-md-block" />
-
-  <!-- Sidebar Toggler (Sidebar) -->
   <div class="text-center d-none d-md-inline">
     <button class="rounded-circle border-0" id="sidebarToggle"></button>
   </div>
-
-  <!-- Sidebar Message -->
-  <div class="sidebar-card d-none d-lg-flex">
-    <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="..." />
-    <p class="text-center mb-2">
-      <strong>SB Admin Pro</strong> is packed with premium features,
-      components, and more!
-    </p>
-    <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-  </div>
 </ul>
-<!-- End of Sidebar -->
